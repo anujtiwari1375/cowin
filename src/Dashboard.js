522b6dc2461bbeeac138e14ddeb712a8f55aa377
anuj <anujtiwari1375@gmail.com>
2021-12-09 13:53:12 +0530

    added changes in application

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -2,6 +2,10 @@ import React from "react";
 import { connect } from 'react-redux';
 import isExists from './functions/isExists';
 import { getPublicStats, getStats, getDistrict } from './actions/DashboardAction';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Doughnut, Pie } from 'react-chartjs-2';
+
+ChartJS.register(ArcElement, Tooltip, Legend);
 
 class Dashboard extends React.Component {
     constructor(props) {
@@ -75,8 +79,108 @@ class Dashboard extends React.Component {
 
     render() {
         const { get_public_stats, get_stats, get_state } = this.props;
-        console.log("get_stats in render function", get_stats);
+        console.log("get_stats in render function", get_public_stats);
+
+
+
+        var male_vacination = isExists(get_public_stats) ? get_public_stats.topBlock.vaccination.male : '';
+        var female_vacination = isExists(get_public_stats) ? get_public_stats.topBlock.vaccination.female : '';
+        var others_vacination = isExists(get_public_stats) ? get_public_stats.topBlock.vaccination.others : '';
+
+        var covishield_vacination = isExists(get_public_stats) ? get_public_stats.topBlock.vaccination.covishield : '';
+        var covaxin_vacination = isExists(get_public_stats) ? get_public_stats.topBlock.vaccination.covaxin : '';
+        var sputnik_vacination = isExists(get_public_stats) ? get_public_stats.topBlock.vaccination.sputnik : '';
+
+        var vac_18_45 = isExists(get_public_stats) ? get_public_stats.vaccinationByAge.vac_18_45 : '';
+        var vac_45_60 = isExists(get_public_stats) ? get_public_stats.vaccinationByAge.vac_45_60 : '';
+        var above_60 = isExists(get_public_stats) ? get_public_stats.vaccinationByAge.above_60 : '';
+
+
         var districtArray = this.state.districtArray;
+        var vcaccination_category_gender = {
+            labels: [
+                "Male",
+                "Female",
+                "Others"
+            ],
+            datasets: [
+                {
+                    data: [male_vacination, female_vacination, others_vacination],
+                    backgroundColor: [
+                        "#5A8DEE",
+                        "#F54394",
+                        "#FF9800"
+                    ],
+                    hoverBackgroundColor: [
+                        "#5A8DEE",
+                        "#F54394",
+                        "#FF9800"
+                    ],
+                    borderRadius: 0,
+                    borderWidth: 2,
+                    circumference: 180,
+                    rotation: 270,
+                    cutoutPercentage: 35,
+                },
+            ]
+        };
+
+        var vcaccination_category_vaccine = {
+            labels: [
+                "Covishield",
+                "Covaxin",
+                "Sputnik"
+            ],
+            datasets: [
+                {
+                    data: [covishield_vacination, covaxin_vacination, sputnik_vacination],
+                    backgroundColor: [
+                        "#007CC3",
+                        "#7AC142",
+                        "#FFF44C"
+                    ],
+                    hoverBackgroundColor: [
+                        "#007CC3",
+                        "#36A2EB",
+                        "#FFF44C"
+                    ],
+                    borderRadius: 0,
+                    borderWidth: 2,
+                    circumference: 180,
+                    rotation: 270,
+                    cutoutPercentage: 35,
+                },
+            ]
+        };
+
+        var vcaccination_category_age = {
+            labels: [
+                "18-44",
+                "45-60",
+                "Above 60"
+            ],
+            datasets: [
+                {
+                    data: [vac_18_45, vac_45_60, above_60],
+                    backgroundColor: [
+                        "#AADEA7",
+                        "#64C2A6",
+                        "#2D87BB"
+                    ],
+                    hoverBackgroundColor: [
+                        "#AADEA7",
+                        "#64C2A6",
+                        "#2D87BB"
+                    ],
+                    borderRadius: 0,
+                    borderWidth: 2,
+                    // circumference: 180,
+                    // rotation: 270,
+                    // cutoutPercentage: 35,
+                },
+            ]
+        };
+
         return (
             <div className="content-wrapper" style={{ "minHeight": '330px' }}>
                 <div className="content liveCounter">
@@ -190,9 +294,10 @@ class Dashboard extends React.Component {
                 {/* Main Section Container */}
                 <div className="content">
                     <div className="container-fluid main-card-block">
-                        {/* Total Vaccination Doses */}
-                        <div className="row">
-                            <div className="col-6 col-sm-12 margin-b cardblockcls col-lg-4">
+
+                        <div className="row mb-4">
+                            {/* Total Vaccination Doses */}
+                            <div className={isExists(get_public_stats) && isExists(get_public_stats.topBlock.registration.total) ? 'col-6 col-sm-12 margin-b cardblockcls col-lg-4' : 'col-6 col-sm-12 margin-b cardblockcls col-lg-6'}>
                                 <div className="small-box bg-white cm-vaccinated-box">
                                     <div className="icon icon-bg-vaccine">
                                         <span className="icon-vaccine"></span>
@@ -216,7 +321,7 @@ class Dashboard extends React.Component {
                                 </div>
                             </div>
                             {/* Site Conducting Vaccination */}
-                            <div className="col-6 col-sm-12 margin-b cardblockcls col-lg-4">
+                            <div className={isExists(get_public_stats) && isExists(get_public_stats.topBlock.registration.total) ? 'col-6 col-sm-12 margin-b cardblockcls col-lg-4' : 'col-6 col-sm-12 margin-b cardblockcls col-lg-6'}>
                                 <div className="small-box bg-white cm-vaccinated-box">
                                     <div className="icon icon-bg-vaccine">
                                         <span className="icon-vaccine"></span>
@@ -240,34 +345,96 @@ class Dashboard extends React.Component {
                                 </div>
                             </div>
                             {/* Total Registration */}
-                            <div className="col-6 col-sm-12 margin-b cardblockcls col-lg-4">
-                                <div className="small-box bg-white cm-vaccinated-box">
-                                    <div className="icon icon-bg-vaccine">
-                                        <span className="icon-vaccine"></span>
-                                    </div>
-                                    <div className="inner innerbox">
-                                        <p className="fontnormal titlemob accessibility-plugin-ac">
-                                            Total Registration
-                                        </p>
-                                        <h3 className="accessibility-plugin-ac">{isExists(get_public_stats) ? get_public_stats.topBlock.registration.total : ''}</h3>
-                                        <div className="row m-0 r-0 w-100">
-                                            <div className="col pl-0">
-                                                <small className="text-muted accessibility-plugin-ac">Age 18-44</small>
-                                                <p className="d-block mb-0 font-weight-bold font-2 accessibility-plugin-ac">{isExists(get_public_stats) ? get_public_stats.topBlock.registration.cit_18_45 : ''}</p>
-                                            </div>
-                                            <div className="col pr-0">
-                                                <small className="text-muted accessibility-plugin-ac">Age 45+</small>
-                                                <p className="d-block mb-0 font-weight-bold font-2 accessibility-plugin-ac">{isExists(get_public_stats) ? get_public_stats.topBlock.registration.cit_45_above : ''}</p>
+                            {isExists(get_public_stats) && isExists(get_public_stats.topBlock.registration.total) ?
+                                <div className="col-6 col-sm-12 margin-b cardblockcls col-lg-4">
+                                    <div className="small-box bg-white cm-vaccinated-box">
+                                        <div className="icon icon-bg-vaccine">
+                                            <span className="icon-vaccine"></span>
+                                        </div>
+                                        <div className="inner innerbox">
+                                            <p className="fontnormal titlemob accessibility-plugin-ac">
+                                                Total Registration
+                                            </p>
+                                            <h3 className="accessibility-plugin-ac">{isExists(get_public_stats) ? get_public_stats.topBlock.registration.total : ''}</h3>
+                                            <div className="row m-0 r-0 w-100">
+                                                <div className="col pl-0">
+                                                    <small className="text-muted accessibility-plugin-ac">Age 18-44</small>
+                                                    <p className="d-block mb-0 font-weight-bold font-2 accessibility-plugin-ac">{isExists(get_public_stats) ? get_public_stats.topBlock.registration.cit_18_45 : ''}</p>
+                                                </div>
+                                                <div className="col pr-0">
+                                                    <small className="text-muted accessibility-plugin-ac">Age 45+</small>
+                                                    <p className="d-block mb-0 font-weight-bold font-2 accessibility-plugin-ac">{isExists(get_public_stats) ? get_public_stats.topBlock.registration.cit_45_above : ''}</p>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
+                                : ''}
+                        </div>
+                        <div className="row">
+                            <div className="col-lg-3 connectedSortable">
+                                <div className="card">
+                                    <div className="card-header">
+                                        Vaccination - Category
+                                    </div>
+                                    <div className="card-body m-auto">
+                                        <Doughnut
+                                            width="350"
+                                            height="175"
+                                            data={vcaccination_category_gender}
+                                            options={{
+                                                responsive: false,
+                                                maintainAspectRatio: false,
+                                            }}
+                                        />
+                                        <div className="mb-3"></div>
+                                        <Doughnut
+                                            width="350"
+                                            height="175"
+                                            data={vcaccination_category_vaccine}
+                                            options={{
+                                                responsive: false,
+                                                maintainAspectRatio: false,
+                                            }}
+                                        />
+                                    </div>
+                                </div>
                             </div>
+                            <div className="col-lg-4 connectedSortable">
 
+                                <div className="card">
+                                    <div className="card-header">Vaccination by Age</div>
+                                    <div className="card-body m-auto">
+                                        <Pie
+                                            width="375"
+                                            height="375"
+                                            data={vcaccination_category_age}
+                                            options={{
+                                                responsive: false,
+                                                maintainAspectRatio: false,
+                                            }}
+                                        />
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div className="col-lg-5 connectedSortable">
+                                <div className="card">
+                                    <div className="card-header">
+                                        Vaccination By State/UT
+                                    </div>
+                                    <div className="card-body">
+
+                                    </div>
+
+                                </div>
+
+
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            </div >
         );
     }
 }
